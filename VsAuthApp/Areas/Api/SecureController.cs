3d1be413caad125556b63df94307b0bd229ec7ca
Yura Horak <yhorak@blackthorn-vision.com>
2019-08-26 01:25:50 +0300

    first demo commit

---
+++ VsAuthApp/Areas/Api/SecureController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VsAuthApp.Api
+{
+    [Route("api/secure")]
+    [ApiController]
+    [Authorize]
+    public class SecureController : ControllerBase
+    {
+        public SecureController()
+        {
+            // lol
+        }
+
+        [HttpGet]
+        public ActionResult<string> GetSecureValue()
+        {
+            return "Super Secure Value";
+        }
+    }
+}
